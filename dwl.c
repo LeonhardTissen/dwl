2037bcefeb50521f2e89625f1e744450356da96f
Leonhard Tissen <contact@warze.org>
2024-08-06 18:47:40 +0000

    grid layout test

--- dwl.c
+++ dwl.c
@@ -2735,6 +2735,41 @@ tile(Monitor *m)
 		i++;
 	}
 }
+ 
+void
+grid(Monitor *m) {
+    unsigned int n = 0, i = 0, ch, cw, rows, cols;
+    Client *c;
+
+    wl_list_for_each(c, &clients, link)
+        if (VISIBLEON(c, m) && !c->isfloating)
+            n++;
+    if (n == 0)
+        return;
+
+    /* grid dimensions */
+    for (rows = 0; rows <= (n / 2); rows++)
+        if ((rows * rows) >= n)
+            break;
+    cols = (rows && ((rows - 1) * rows) >= n) ? rows - 1 : rows;
+
+    /* window geoms (cell height/width) */
+    ch = m->w.height / (rows ? rows : 1);
+    cw = m->w.width / (cols ? cols : 1);
+    wl_list_for_each(c, &clients, link) {
+        unsigned int cx, cy, ah, aw;
+        if (!VISIBLEON(c, m) || c->isfloating || c->isfullscreen)
+            continue;
+
+        cx = m->w.x + (i / rows) * cw;
+        cy = m->w.y + (i % rows) * ch;
+        /* adjust height/width of last row/column's windows */
+        ah = (((i + 1) % rows) == 0) ? m->w.height - ch * rows : 0;
+        aw = (i >= (rows * (cols - 1))) ? m->w.width - cw * cols : 0;
+        resize(c, (struct wlr_box) { .x =  cx, .y = cy, .width = cw - aw, .height = ch - ah }, 0);
+        i++;
+    }
+}
 
 void
 togglefloating(const Arg *arg)
